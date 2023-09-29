ad70d0e6641c37f2bf7c1346ccbc41ae4fad10fc
Tobiasverbeek <302747159@student.rocmondriaan.nl>
2023-09-29 13:13:50 +0200

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,50 @@
+//opdracht 1
+
+let number = 0;
+while (number < 10) {
+    console.log(number);
+    number++;
+}
+    console.log('einde programma')
+
+//opdracht 2
+
+let number1 = 2;
+while (number1 < 21) {
+    console.log(number1);
+    number1++;
+}
+    console.log('einde programma')
+
+//opdracht 3
+
+let number2 = 0;
+while (number2 < 10) {
+    console.log(number2);
+    number2++;
+}
+    console.log('einde programma')
+
+//opdracht 4
+let number3 = 4;
+while (number3 < 40) {
+    console.log(number3);
+    number3++;
+}
+    console.log('einde programma')
+
+//opdracht 5
+let number4 = 0
+while (number4 < 8) {
+    console.log('Ik ben een student van ROC Mondriaan');
+    number4++;
+}
+    console.log('einde programma')
+
+    //opdracht 6
+
+
+
+
+
+
